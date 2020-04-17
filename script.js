db3e97bb934148ecf6c9d9fbf205e6f1785756eb
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-04-17 14:32:02 +0200

    device width compability

--- script.js
+++ script.js
@@ -33,6 +33,16 @@ function changeText() {
     document.getElementById("contactinfo").innerHTML = "Email address copied. Now send me pics of cute dogs! 🐶";
 }
 
-$( document ).ready(function(){
-    var clipboard = new Clipboard('.clipboard');
+var clipboardDemos = new ClipboardJS('[data-clipboard-demo]');
+clipboardDemos.on('success', function(e) {
+    e.clearSelection();
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    showTooltip(e.trigger, 'Copied!');
+});
+clipboardDemos.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+    showTooltip(e.trigger, fallbackMessage(e.action));
 });
