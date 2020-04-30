602197c7b01418d9ae456de3bc2ea89b446cb5d7
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-05-01 00:40:29 +0200

    Finishing  up

--- script/application.js
+++ script/application.js
@@ -76,21 +76,54 @@ $(document).ready(function() {
     // START SCREEN
     $('.color-button').on('click', function(event) {
       var color = $('.start-screen').css('background-color');
-      console.log(color);
+      var newColor;
+      var num = Math.floor(Math.random() * 7);
 
-      switch(color) {
-        case 'rgb(255, 255, 255)':
-          $('.start-screen').css('background-color','#eeca00');
-          color = '#eeca00';
+      switch(num) {
+        case 0:
+          newColor = 'rgb(238, 202, 0)';
         break;
-        case 'rgb(238, 202, 0)':
-          $('.start-screen').css('background-color','rgb(255, 255, 255)');
-          color = 'rgb(255, 255, 255)';
+        case 1:
+          newColor = 'rgb(70, 88, 168)';
+        break;
+        case 2:
+          newColor = 'rgb(197, 71, 71)';
+        break;
+        case 3:
+          newColor = 'rgb(23, 109, 23)';
+        break;
+        case 4:
+          newColor = 'rgb(138, 84, 56)';
+        break;
+        case 5:
+          newColor = 'rgb(199, 179, 255)';
+        break;
+        case 6:
+          newColor = 'rgb(228, 140, 58)';
         break;
       }
 
+      if (color == newColor) {
+          newColor = 'rgb(131, 231, 240)';
+      }
+
+      $('.start-screen').css('background-color', newColor);
+      $('.button-text').css('color', newColor);
+
+      $('.color-button').hover(function() {
+        $('.button-text').css('color', newColor);
+      });
+
+      $('.color-button').mouseleave(function() {
+        $('.button-text').css('color', '#000');
+      });
+
       var tab = $('meta[name=theme-color]');
-      tab.attr('content', color);
+      tab.attr('content', newColor);
+
+      $('body').css('background-color', newColor);
+
+      $('.button-text').text('Yeee! Now scroll down to see my work!');
     });
 
 });
--- script/colourpicker.js
+++ script/colourpicker.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     var color = $('.project-cover').attr("data-color");
 
     $('.project-cover').css("background-color", color);
+    $('.end-bar').css('background-color', color);
     $('.article > pre').css("background-color", opacityChanger(color));
     
     switch(color) {
         case '#dadfd6': 
             $('.article a').css('color', '#72756f');
+            $('.end-bar h2').css('color', '#000000')
         break;
         default:
             $('.article a').css('color', color);
@@ -18,7 +20,7 @@ function opacityChanger(extColor) {
     var rgbaCol = 'rgba(' + parseInt(color.slice(-6,-4),16)
     + ',' + parseInt(color.slice(-4,-2),16)
     + ',' + parseInt(color.slice(-2),16)
-    +',0.25)';
+    +',0.15)';
 
     return rgbaCol;
 };
